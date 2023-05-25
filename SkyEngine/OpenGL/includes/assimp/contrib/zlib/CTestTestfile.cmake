3da37a158944d0a6b906f3595924c5bb11acb2e4
qduke <qduke@DUKE_LAPTOP>
2023-05-25 12:13:41 -0600

    Woah!! Thats Alot!!

---
+++ SkyEngine/OpenGL/includes/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/qduke/assimp/contrib/zlib
+# Build directory: C:/Users/qduke/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
